8d3d8b6b9da943b722d7303437b3b5ffaff3304e
Jarosław Terejko <j.terejko@gmail.com>
2019-09-23 08:23:56 +0200

    converted SearchForm and userDetails class Components to functional Components, implemented hooks

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -1,36 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export default class SearchForm extends Component {
-  state = {
-    search: ""
+const SearchForm = ({ showAlert, searchUsers, showClearBtn, clearUsers }) => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = e => {
+    setSearch(e.target.value);
   };
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClearBtn: PropTypes.bool.isRequired,
-    showAlert: PropTypes.func.isRequired
-  };
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.search === "") {
-      this.props.showAlert("Please enter text to search", "red");
+    if (search === "") {
+      showAlert("Please enter text to search", "red");
     } else {
-      this.props.searchUsers(this.state.search);
-      this.setState({ search: "" });
+      searchUsers(search);
+      setSearch("");
     }
   };
-  render() {
-    return (
-      <div className="row" style={{ marginTop: "30px" }}>
+
+  return (
+    <div className="row" style={{ marginTop: "30px" }}>
       <div className="col s12">
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="input-field col s12">
               <input
@@ -38,7 +29,7 @@ export default class SearchForm extends Component {
                 id="search"
                 type="text"
                 className="validate"
-                onChange={this.handleChange}
+                onChange={handleChange}
               />
               <label htmlFor="search">Search Users...</label>
             </div>
@@ -46,17 +37,25 @@ export default class SearchForm extends Component {
           <button className="btn" type="submit">
             Search
           </button>
-          {this.props.showClearBtn && (
+          {showClearBtn && (
             <button
               className="btn waves-effect waves-light"
-              onClick={this.props.clearUsers}
+              onClick={clearUsers}
             >
               Clear
             </button>
           )}
         </form>
-        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+SearchForm.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClearBtn: PropTypes.bool.isRequired,
+  showAlert: PropTypes.func.isRequired
+};
+
+export default SearchForm;
--- src/Components/UserDetails/UserDetails.js
+++ src/Components/UserDetails/UserDetails.js
@@ -1,156 +1,155 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, Fragment } from "react";
 import Loader from "../Loader/Loader";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./UserDetails.css";
 import Repos from "../Repos/Repos";
 
-export default class UserDetails extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
+const UserDetails = ({
+  user,
+  loading,
+  getUser,
+  getUserRepos,
+  repos,
+  match
+}) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired
-  };
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user;
 
-  render() {
-    const {
-      name,
-      company,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = this.props.user;
+  if (loading) {
+    return <Loader />;
+  } else {
+    return (
+      <Fragment>
+        <div className="row">
+          <div className="col s12">
+            <div style={{ marginTop: "15px", marginBottom: "15px" }}>
+              <Link className="waves-effect waves-light btn" to="/">
+                <i className="material-icons left">keyboard_arrow_left</i>Back
+                To Search
+              </Link>
+            </div>
+            <div className="card">
+              <div
+                className="card-image"
+                style={{
+                  paddingTop: "24px",
+                  paddingLeft: "24px",
+                  paddingRight: "24px"
+                }}
+              >
+                <img src={avatar_url} style={{ width: "150px" }} alt="User" />
+                <h5>{name}</h5>
+                <p>
+                  Location: <strong>{location}</strong>
+                </p>
 
-    const { loading, repos } = this.props;
-
-    if (loading) {
-      return <Loader />;
-    } else {
-      return (
-        <Fragment>
-          <div className="row">
-            <div className="col s12">
-              <div style={{ marginTop: "15px", marginBottom: "15px" }}>
-                <Link className="waves-effect waves-light btn" to="/">
-                  <i className="material-icons left">keyboard_arrow_left</i>Back
-                  To Search
-                </Link>
-              </div>
-              <div className="card">
-                <div
-                  className="card-image"
-                  style={{
-                    paddingTop: "24px",
-                    paddingLeft: "24px",
-                    paddingRight: "24px"
-                  }}
-                >
-                  <img src={avatar_url} style={{ width: "150px" }} alt="User" />
-                  <h5>{name}</h5>
-                  <p>
-                    Location: <strong>{location}</strong>
-                  </p>
-
-                  <p>
-                    Hireable:{" "}
-                    {hireable ? (
-                      <i className="fas fa-check green-text"></i>
-                    ) : (
-                      <i className="fas fa-times red-text "></i>
-                    )}
-                  </p>
-                  <div className="badges-container">
-                    {followers && (
-                      <span
-                        className="my-badge brown darken-1"
-                        style={{ color: "#fff" }}
-                      >
-                        Followers: {followers}
-                      </span>
-                    )}
-                    {following && (
-                      <span className="my-badge red" style={{ color: "#fff" }}>
-                        Following: {following}
-                      </span>
-                    )}
-                    {public_repos && (
-                      <span
-                        className="my-badge purple"
-                        style={{ color: "#fff" }}
-                      >
-                        Repos: {public_repos}
-                      </span>
-                    )}
-                    {public_gists && (
-                      <span className="my-badge cyan" style={{ color: "#fff" }}>
-                        Gist: {public_gists}
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="card-content">
-                  {bio && (
-                    <Fragment>
-                      <h5>Bio</h5>
-                      <p style={{ paddingBottom: "20px" }}>{bio}</p>
-                    </Fragment>
+                <p>
+                  Hireable:{" "}
+                  {hireable ? (
+                    <i className="fas fa-check green-text"></i>
+                  ) : (
+                    <i className="fas fa-times red-text "></i>
                   )}
-                  <ul className="collection">
-                    {login && (
-                      <li className="collection-item">
-                        Username: <strong>{login}</strong>
-                      </li>
-                    )}
-                    {company && (
-                      <li className="collection-item">
-                        Company: <strong>{company}</strong>
-                      </li>
-                    )}
-                    {blog && (
-                      <li className="collection-item">
-                        Web:{" "}
-                        <a
-                          href={blog}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <strong>{blog}</strong>
-                        </a>
-                      </li>
-                    )}
-                  </ul>
-                  <Repos repos={repos} />
+                </p>
+                <div className="badges-container">
+                  {followers && (
+                    <span
+                      className="my-badge brown darken-1"
+                      style={{ color: "#fff" }}
+                    >
+                      Followers: {followers}
+                    </span>
+                  )}
+                  {following && (
+                    <span className="my-badge red" style={{ color: "#fff" }}>
+                      Following: {following}
+                    </span>
+                  )}
+                  {public_repos && (
+                    <span className="my-badge purple" style={{ color: "#fff" }}>
+                      Repos: {public_repos}
+                    </span>
+                  )}
+                  {public_gists && (
+                    <span className="my-badge cyan" style={{ color: "#fff" }}>
+                      Gist: {public_gists}
+                    </span>
+                  )}
                 </div>
-                <div className="card-action">
-                  <a
-                    href={html_url}
-                    className="brown-text"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    GitHub Profile
-                  </a>
-                </div>
+              </div>
+              <div className="card-content">
+                {bio && (
+                  <Fragment>
+                    <h5>Bio</h5>
+                    <p style={{ paddingBottom: "20px" }}>{bio}</p>
+                  </Fragment>
+                )}
+                <ul className="collection">
+                  {login && (
+                    <li className="collection-item">
+                      Username: <strong>{login}</strong>
+                    </li>
+                  )}
+                  {company && (
+                    <li className="collection-item">
+                      Company: <strong>{company}</strong>
+                    </li>
+                  )}
+                  {blog && (
+                    <li className="collection-item">
+                      Web:{" "}
+                      <a href={blog} target="_blank" rel="noopener noreferrer">
+                        <strong>{blog}</strong>
+                      </a>
+                    </li>
+                  )}
+                </ul>
+                <Repos repos={repos} />
+              </div>
+              <div className="card-action">
+                <a
+                  href={html_url}
+                  className="brown-text"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub Profile
+                </a>
               </div>
             </div>
           </div>
-        </Fragment>
-      );
-    }
+        </div>
+      </Fragment>
+    );
   }
-}
+};
+
+UserDetails.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired
+};
+
+export default UserDetails;
